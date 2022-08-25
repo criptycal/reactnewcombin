370f133ce86988330c472375bf1c7b2160f3b835
David Salamanca <criptse@gmail.com>
2022-08-25 09:39:12 -0500

    hot fix

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -118,24 +118,28 @@ export default function Filters({ typeBtn }: FilterProps) {
 
                         <span className="sm:ml-3">
                             {typeBtn === 'createPayable' &&
-                                <Link
+                            <Link href="/payables/create">
+                                <a
                                     type="button"
                                     className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    href="/payables/create"
+                                    
                                 >
                                     <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                                     Create Payable
-                                </Link>
+                                </a>
+                            </Link>
                             }
                             {typeBtn === 'Payable' &&
-                                <Link
+                            <Link href="/payables">
+                                <a
                                     type="button"
                                     className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    href="/payables"
+                                    
                                 >
                                     <ArrowLeftIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                                     Volver
-                                </Link>
+                                </a>
+                            </Link>
                             }
                         </span>
 
